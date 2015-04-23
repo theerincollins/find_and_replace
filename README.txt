1ad343709526ae2afaba527dd55bdf3924b48760
Erin & Peter <student@epicodus.com>
2015-04-22 17:05:06 -0700

    broke function on part two of exercise

--- README.txt
+++ README.txt
@@ -1,4 +1,8 @@
 Find and Replace
 
 User inputs three text strings. The first text string will be the receiver.
- The other two text string will be arguments in a ruby method. 
+ The other two text string will be arguments in a ruby method.
+
+ Do second: Add to your method, so that your method takes into account partial matches.
+  For example "I am walking my cat to the cathedral".find_and_replace("cat", "dog") would
+  return the silly phrase "I am walking my dog to the doghedral."
--- lib/f_and_r.rb
+++ lib/f_and_r.rb
@@ -1,11 +1,22 @@
 class String
   def f_and_r (chosen_word, replaced_word)
-
+  chosen_word_split = chosen_word.split(//)
+  replaced_word_split = replaced_word.split(//)
   input_string = self.split(" ")
   final_string = []
+  word_to_replace = []
+  second_part_replaced_word = replaced_word.split(/replaced_word_split.at(0)/)
+
+   a = second_part_replaced_word.join()
+   b = []
+   b.push(a)
 
   input_string.each do |word|
-    if word == chosen_word
+    if word.include?(chosen_word_split.at(0))
+      word_letter = word.split()
+      location = word_letter.index(chosen_word_split.at(0))
+      if word_letter.include?(chosen_word_split.at(location+1))
+        word_letter.at(location).replace(replaced_word_split.at(0))
       word.replace(replaced_word)
     else
       word
@@ -14,5 +25,8 @@ class String
   end
  final_string.join(" ")
 
+
+
   end
+end
 end
